b510a9c49de590a027214683c277346d6f19f648
Sivan <sivan.syed@gmail.com>
2025-09-28 22:55:57 -0500

    set site name

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -3,7 +3,7 @@ import svelte from '@astrojs/svelte';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://sivansyed.github.io',   // your GH Pages root
+  site: 'https://ilgneous.github.io/sivansyed.github.io/',   // your GH Pages root
   base: '/sivansyed.github.io/',                      // <-- set to your repo name
   integrations: [svelte(), tailwind()],
 });
